fc5db31968f934bcd8ff7a33e50765d2388829de
Roy Storey <kiwiroy@users.noreply.github.com>
2018-03-27 14:35:38 +1300

    using pkgload internals

--- R/usethis-package.R
+++ R/usethis-package.R
@@ -3,4 +3,11 @@
 
 .onLoad <- function(libname, pkgname) {
   backports::import(pkgname, "dir.exists")
+
+  if("devtools::test" %in% sys.calls()[1]) {
+    ns <- asNamespace(pkgname)
+    impenv <- parent.env(ns)
+    pkgload:::unlock_environment(impenv)
+    pkgload:::insert_imports_shims(pkgname)
+  }
 }
